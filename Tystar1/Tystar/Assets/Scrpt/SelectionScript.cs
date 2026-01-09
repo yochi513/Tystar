e27e089e7bac84358f69748730515f761ca46e91
yochi513 <yojrdkt@gmail.com>
2026-01-10 02:23:23 +0900

    説明追加とボール速度調整

--- Tystar1/Tystar/Assets/Scrpt/SelectionScript.cs
+++ Tystar1/Tystar/Assets/Scrpt/SelectionScript.cs
@@ -9,14 +9,17 @@ public class SelectionScript : MonoBehaviour
     [SerializeField] Button start;
     [SerializeField] Button sutez;
     [SerializeField] Button cancel;
+    [SerializeField] Button Setumei1;
     [SerializeField] Canvas START;
     [SerializeField] Canvas Select;
+    [SerializeField] Canvas Setumei2;
 
     // Start is called before the first frame update
     void Start()
     {
-    Select.gameObject.SetActive(false);
-    START.gameObject.SetActive(true);
+        Select.gameObject.SetActive(false);
+        Setumei2.gameObject.SetActive(false);
+        START.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -28,19 +31,30 @@ public class SelectionScript : MonoBehaviour
     public void kaisi()
     {
         Select.gameObject.SetActive(true);
+        Setumei2.gameObject.SetActive(false);
         START.gameObject.SetActive(false);
+
     }
     public void modoru()
     {
         Select.gameObject.SetActive(false);
+        Setumei2.gameObject.SetActive(false);
         START.gameObject.SetActive(true);
     }
 
-    public void SELECT()
+    public void SELECTEASY()
     {
         SceneManager.LoadScene("MainScene");
     }
-
-
+    public void Setumei()
+    {
+        Select.gameObject.SetActive(false);
+        Setumei2.gameObject.SetActive(true);
+    }
+    public void Setumeimodoru()
+    {
+        Select.gameObject.SetActive(true);
+        Setumei2.gameObject.SetActive(false);
+    }
 
 }
